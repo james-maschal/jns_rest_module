6145ff25624935d2d1885d6b0bded632e08916d1
james maschal <netmaschal@bdcnetstats1.tch.harvard.edu>
2023-07-17 13:20:26 -0400

    minor adjustment to code

--- rest_module.py
+++ rest_module.py
@@ -4,14 +4,11 @@ requests.packages.urllib3.disable_warnings()
 
 def rest_connect(device, container, headers):
 
-#auth_var,
-
       url = f"https://{device}{container}"
 
       try:
             response = requests.get(
                                     url,
-                                   # auth=auth_var,
                                     headers=headers,
                                     verify=False,
                                     timeout=3)
